2a348014871dbb7f89141deae7e3e78e4e18c174
Damir9713 <89656393+Damir9713@users.noreply.github.com>
2022-03-23 02:19:24 +0100

    Security image

--- action/establishment/editEstablishment.php
+++ action/establishment/editEstablishment.php
@@ -1,10 +1,11 @@
-<?php 
+  <?php 
 
 require("action/database.php");
 
 $photo = "";
 
 if(isset($_POST['valider'])){
+
 
   if(!empty($_POST['name'])
     AND !empty($_POST['city']) 
@@ -20,12 +21,14 @@ if(isset($_POST['valider'])){
 
     $type_file = $_FILES['images']['type'];     
     if( !strstr($type_file, 'jpg') && !strstr($type_file, 'jpeg') && !strstr($type_file, 'bmp') && !strstr($type_file, 'gif') ) {     
-       exit("Le fichier n'est pas une image");     
+      exit ("Le fichier n'est pas une image");
     }
-
+    
     $photo = $_FILES['images']['name'];
-    $upload = "upload/".$photo;
-
+    $extensions = ['png', 'jpg', 'jpeg'];
+    $uniqueName = uniqid('', true);
+    $file = $uniqueName.".".strtolower(end($extensions));
+    $upload = "upload/".$file;
     move_uploaded_file($_FILES['images']['tmp_name'], $upload);
 
     $insertImage = $bdd->prepare('UPDATE establishment 
@@ -42,14 +45,16 @@ $insertImage->execute(
      $city,
      $adress,
      $description,
-     $photo,
+     $file,
      $idOfEstablishment
     )
 );
 $sucessMsg = "Votre établissement a bien été ajoutée ";
 header('Location: establishmentAdmin.php');
+  
 }else{
   $errorMsg = "Veuillez remplir tout les champs";
 }
 }
-?>
+?>  
+
--- editEstablishment.php
+++ editEstablishment.php
@@ -2,7 +2,7 @@
 session_start();
 require('action/securityAdmin.php');
 require('action/establishment/getInfoOfEditedEstablishment.php');
-require('action/establishment/editEstablishment.php');
+// require('action/establishment/editEstablishment.php');
 
 ?>
 
@@ -17,7 +17,9 @@ require('action/establishment/editEstablishment.php');
 
     <br><br>
     <div class="container">
-        <?php if(isset($errorMsg)){ echo '<p>'.$errorMsg.'</p>'; } ?>
+        <?php if(isset($errorMsg)){ echo '<p>'.$errorMsg.'</p>';} 
+
+        ?>
         
         <?php 
             if(isset($_SESSION['id_admin'])){ 
@@ -42,18 +44,81 @@ require('action/establishment/editEstablishment.php');
             
             <div class="mb-3">
               <input type="file" name="images">
+
             </div>
-       
+            
+            
+    
+            
                     <button type="submit" class="btn btn-outline-warning" name="valider">Modifier l'établissement</button>
                     <br><br>
                 </form>
+
                 
                 <?php
             }
         ?>
-        
 
 
+<?php 
+
+require("action/database.php");
+
+$photo = "";
+
+if(isset($_POST['valider'])){
+
+  if(!empty($_POST['name'])
+    AND !empty($_POST['city']) 
+    AND !empty($_POST['adress'])  
+    AND !empty($_POST['description']) 
+  ){
+
+    $name = htmlspecialchars($_POST['name']);
+    $description = nl2br(htmlspecialchars($_POST['description']));
+    $city = htmlspecialchars($_POST['city']);
+    $adress = htmlspecialchars($_POST['adress']);
+
+
+    $type_file = $_FILES['images']['type'];     
+    if( !strstr($type_file, 'jpg') && !strstr($type_file, 'jpeg') && !strstr($type_file, 'bmp') && !strstr($type_file, 'gif') ) {    
+      exit ("Le fichier n'est pas une image");
+    }
+    
+    $photo = $_FILES['images']['name'];
+    $extensions = ['png', 'jpg', 'jpeg'];
+    $uniqueName = uniqid('', true);
+    $file = $uniqueName.".".strtolower(end($extensions));
+    $upload = "upload/".$file;
+    move_uploaded_file($_FILES['images']['tmp_name'], $upload);
+
+    $insertImage = $bdd->prepare('UPDATE establishment 
+    SET name = ?, 
+    city = ?, 
+    adress = ?, 
+    description = ?, 
+    photo = ?  
+    WHERE id = ?');
+
+$insertImage->execute(
+    array(
+     $name,
+     $city,
+     $adress,
+     $description,
+     $file,
+     $idOfEstablishment
+    )
+);
+$sucessMsg = "Votre établissement a bien été ajoutée ";
+header('Location: establishmentAdmin.php');
+  
+}else{
+  $errorMsg = "Veuillez remplir tout les champs";
+}
+}
+
+?>  
         
                         
     </div>
@@ -61,4 +126,5 @@ require('action/establishment/editEstablishment.php');
 
 </body>
 </html>
+
 
