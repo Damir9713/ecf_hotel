39c1f32ffe7561421983c097665e189f14375aa1
Damir9713 <89656393+Damir9713@users.noreply.github.com>
2022-03-26 01:07:22 +0100

    Update addSuite.php

--- addSuite.php
+++ addSuite.php
@@ -3,60 +3,8 @@ session_start();
 require('action/securityManager.php');
 // require('action/suite/addSuiteAction.php');
 require 'vendor/autoload.php';
-?>
-<!DOCTYPE html>
-<html lang="en">
-<?php include 'includes/head.php'; ?>
-<body>
-<?php include 'includes/navbar.php'; ?>
-<br><br>
 
-<div class="container">
-<?php 
-            if(isset($errorMsg)){ 
-                echo '<p>'.$errorMsg.'</p>'; 
-            }elseif(isset($successMsg)){ 
-                echo '<p>'.$successMsg.'</p>'; 
-            }
-        ?>
-</div>
 
-<div class="container">
-
-<form    method="post" enctype="multipart/form-data">
-      <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Ttire</label>
-                <input type="text" class="form-control" name="title">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Prix</label>
-                <input type="text" class="form-control" name="price" >
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">description</label>
-                <textarea type="email" class="form-control" name="description" ></textarea>
-            </div>
-            
-            <div class="mb-3">
-              <input type="file" name="firstphoto">
-              1 ère image carousel
-            </div>
-            <div class="mb-3">
-              <input type="file" name="secondphoto">
-              2 ème image carousel
-            </div>
-            <div class="mb-3">
-              <input type="file" name="thirdphoto">
-              3 ème image carousel
-            </div>
-      
-            <button type="submit" class="btn btn-outline-warning" name="valider">Ajouter une suite</button>
-            </div>
-    </form>
-
-    <?php
-
-// se connecter à la bdd
 require('action/database.php');
 
 //Valider le formulaire
@@ -202,6 +150,62 @@ if(isset($_POST['valider'])){
     }
  
     ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'includes/head.php'; ?>
+<body>
+<?php include 'includes/navbar.php'; ?>
+<br><br>
+
+<div class="container">
+<?php 
+            if(isset($errorMsg)){ 
+                echo '<p>'.$errorMsg.'</p>'; 
+            }elseif(isset($successMsg)){ 
+                echo '<p>'.$successMsg.'</p>'; 
+            }
+        ?>
+</div>
+
+<div class="container">
+
+<form    method="post" enctype="multipart/form-data">
+      <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Ttire</label>
+                <input type="text" class="form-control" name="title">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Prix</label>
+                <input type="text" class="form-control" name="price" >
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">description</label>
+                <textarea type="email" class="form-control" name="description" ></textarea>
+            </div>
+            
+            <div class="mb-3">
+              <input type="file" name="firstphoto">
+              1 ère image carousel
+            </div>
+            <div class="mb-3">
+              <input type="file" name="secondphoto">
+              2 ème image carousel
+            </div>
+            <div class="mb-3">
+              <input type="file" name="thirdphoto">
+              3 ème image carousel
+            </div>
+      
+            <button type="submit" class="btn btn-outline-warning" name="valider">Ajouter une suite</button>
+            </div>
+    </form>
+
+    
+
+
+
 
 </div>
 
